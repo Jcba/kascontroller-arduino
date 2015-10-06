6331c4d806b9b031b9da3b387520d9773240954c
Job  Bakker <job190@gmail.com>
2015-10-06 20:02:11 +0000

    started working on the display interface code

---
+++ watering/Display.h
@@ -0,0 +1,20 @@
+/*
+ * Display.h
+ *
+ *  Created on: Oct 6, 2015
+ *      Author: Job Bakker
+ *
+ *  Display code
+ */
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+class Display {
+public:
+  Display();
+  
+  
+};
+
+#endif /* DISPLAY_H_ */
